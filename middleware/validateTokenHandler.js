003515349a9e790cbb68ec3b90b3ddf050064876
Bashar Subh <133267331+BasharSubh@users.noreply.github.com>
2023-07-31 18:28:03 +0100

    Update validateTokenHandler.js
    add a check for the session when its expired and console the error to inform the user

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -3,23 +3,23 @@ const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
   let token;
-  let authHeader = req.headers.Authorization || req.headers.authorization;
-  if (authHeader && authHeader.startsWith("Bearer")) {
-    token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        res.status(401);
-        throw new Error("User is not authorized");
-      }
-      req.user = decoded.user;
-      next();
-    });
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
-    if (!token) {
-      res.status(401);
-      throw new Error("User is not authorized or token is missing");
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    token = authHeader.split(' ')[1];
+    try {
+      const decoded = jwt.verify(token, process.env.TOKEN);
+      req.user = decoded.user;
+      return next();
+    } catch (err) {
+      if (err.message === "jwt expired") {
+        return res.status(401).json({ error: 'Your session has expired. Please login again.' });
+      }
+      return res.status(401).json({ error: 'User is not authorized.' });
     }
   }
-});
+
+  res.status(401).json({ error: 'User is not authorized.' });
+};
 
 module.exports = validateToken;
