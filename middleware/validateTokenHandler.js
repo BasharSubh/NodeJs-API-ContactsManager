d9bd541e46ba2c31afdfedf2c890a6ef89af1c5a
Bashar Subh <133267331+BasharSubh@users.noreply.github.com>
2023-07-31 19:19:15 +0100

    Update validateTokenHandler.js
    instead of return the response with the error , we throw the error, to inform the user when the session expired

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -7,6 +7,10 @@ const validateToken = asyncHandler(async (req, res, next) => {
   if (authHeader && authHeader.startsWith("Bearer")) {
     token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+       if (err.message === "jwt expired") {
+         res.status(401);
+        throw new Error("Your session has expired. Please login again.")
+            }
       if (err) {
         res.status(401);
         throw new Error("User is not authorized");
